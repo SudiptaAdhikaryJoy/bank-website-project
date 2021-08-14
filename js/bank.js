db990d4bcbbf316f4ce49475cf81642e9d19dd98
SudiptaAdhikaryJoy <dipto5russow@gmail.com>
2021-08-15 00:46:48 +0600

    first commit

---
+++ js/bank.js
@@ -0,0 +1,21 @@
+document.getElementById('login-submit').addEventListener('click', function() {
+    // console.log('clicked!!');
+
+    // get user-email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    // console.log(userEmail);
+
+    // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    // console.log(userPassword);
+   
+    // check email and password
+    if(userEmail == 'sontan@baap.com' && userPassword == 'hola'){
+        window.location.href="banking.html";
+    }
+
+
+
+})
